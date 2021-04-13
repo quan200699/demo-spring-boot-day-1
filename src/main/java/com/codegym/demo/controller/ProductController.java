c64147230c59b20b3ab325150c18510e6a0faf82
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-04-13 18:16:06 +0700

    upload file done

--- src/main/java/com/codegym/demo/controller/ProductController.java
+++ src/main/java/com/codegym/demo/controller/ProductController.java
@@ -1,12 +1,18 @@
 package com.codegym.demo.controller;
 
 import com.codegym.demo.model.Product;
+import com.codegym.demo.model.ProductForm;
 import com.codegym.demo.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -14,6 +20,9 @@ import java.util.Optional;
 public class ProductController {
     @Autowired
     private IProductService productService;
+
+    @Value(value = "${upload.path}")
+    private String fileUpload;
 
     @GetMapping("/list")
     public ModelAndView showAll() {
@@ -26,15 +35,27 @@ public class ProductController {
     @GetMapping("/create")
     public ModelAndView showFormCreate() {
         ModelAndView modelAndView = new ModelAndView("/product/create");
-        modelAndView.addObject("product", new Product());
+        modelAndView.addObject("product", new ProductForm());
         return modelAndView;
     }
 
     @PostMapping("/create")
-    public ModelAndView createNewProduct(@ModelAttribute Product product) {
+    public ModelAndView createNewProduct(@ModelAttribute ProductForm productForm) {
+        Product product = new Product();
+        MultipartFile multipartFile = productForm.getImage();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(productForm.getImage().getBytes(), new File(this.fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        product.setName(productForm.getName());
+        product.setPrice(productForm.getPrice());
+        product.setDescription(productForm.getDescription());
+        product.setImgUrl(fileName);
         productService.save(product);
         ModelAndView modelAndView = new ModelAndView("/product/create");
-        modelAndView.addObject("product", new Product());
+        modelAndView.addObject("product", new ProductForm());
         return modelAndView;
     }
 
---
+++ src/main/java/com/codegym/demo/model/ProductForm.java
@@ -0,0 +1,21 @@
+package com.codegym.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductForm {
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private String description;
+
+    private MultipartFile image;
+}
